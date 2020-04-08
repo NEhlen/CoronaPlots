676eaaf925f81ce3133901024eabd30cf3ce0e8c
MagiMas <magimas90@googlemail.com>
2020-04-08 17:29:51 +0200

    include option for deaths

--- download.py
+++ download.py
@@ -3,6 +3,8 @@ import os, sys
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+url_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
-urllib.request.urlretrieve(url, './corona_time_series.csv')
+urllib.request.urlretrieve(url_confirmed, './corona_time_series.csv')
+urllib.request.urlretrieve(url_deaths, './corona_time_series_deaths.csv')
--- main.py
+++ main.py
@@ -9,13 +9,13 @@ os.chdir(os.path.dirname(sys.argv[0]))
 print(sys.argv)
 
 # load data from file
-file_ = "./corona_time_series.csv"
+file_ = sys.argv[1] if len(sys.argv)>1 else "./corona_time_series.csv"
 
 # how many days should be averaged over, either take from command line arguments if provided or use 5
-avg_days = int(sys.argv[1]) if len(sys.argv)>1 else 5
+avg_days = int(sys.argv[2]) if len(sys.argv)>2 else 5
 
 # which countries should be displayed, take from n last command line arguments if provided, else use Italy, US and Germany
-country_list_ = sys.argv[2:len(sys.argv)] if len(sys.argv)>2 else ["Italy", "US", "Germany"]
+country_list_ = sys.argv[3:len(sys.argv)] if len(sys.argv)>3 else ["Italy", "US", "Germany"]
 
 # print list of countries displayed
 print(country_list_)
@@ -45,18 +45,35 @@ for i_c, cntry in zip(iList, country_list_):
     ax2.plot(range(len(y)),y/avg_days,"o--", label=cntry,markersize=3, linewidth=1, fillstyle='none')
 
 # set graph options
-ax.set_ylabel('Neue Patienten letzte %d Tage' % avg_days)
-ax.set_xlabel('Patienten Total')
-ax.legend()
-fig.tight_layout()
-fig.savefig('NeuePatienten-PatientenTotal_%dTage.png'%avg_days)
-ax.set_yscale("log")
-ax.set_xscale("log")
-fig.tight_layout()
-fig.savefig('NeuePatienten-PatientenTotal_%dTage_loglog.png'%avg_days)
+if file_ == "./corona_time_series.csv":
+    ax.set_ylabel('Neue Patienten letzte %d Tage' % avg_days)
+    ax.set_xlabel('Patienten Total')
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig('NeuePatienten-PatientenTotal_%dTage.png'%avg_days)
+    ax.set_yscale("log")
+    ax.set_xscale("log")
+    fig.tight_layout()
+    fig.savefig('NeuePatienten-PatientenTotal_%dTage_loglog.png'%avg_days)
 
-ax2.set_ylabel("$\\varnothing$ neue Patienten pro Tag")
-ax2.set_xlabel("Tage nach 100 erreichten Fällen")
-ax2.legend()
-fig2.tight_layout()
-fig2.savefig('NeuePatienten-Tage_%dTageDurchschnitt.png'%avg_days)
+    ax2.set_ylabel("$\\varnothing$ neue Patienten pro Tag")
+    ax2.set_xlabel("Tage nach 100 erreichten Fällen")
+    ax2.legend()
+    fig2.tight_layout()
+    fig2.savefig('NeuePatienten-Tage_%dTageDurchschnitt.png'%avg_days)
+else:
+    ax.set_ylabel('Neue Tote letzte %d Tage' % avg_days)
+    ax.set_xlabel('Tote Total')
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig('NeueTote-ToteTotal_%dTage.png'%avg_days)
+    ax.set_yscale("log")
+    ax.set_xscale("log")
+    fig.tight_layout()
+    fig.savefig('NeueTote-ToteTotal_%dTage_loglog.png'%avg_days)
+
+    ax2.set_ylabel("$\\varnothing$ neue Tote pro Tag")
+    ax2.set_xlabel("Tage nach 100 erreichten Toten")
+    ax2.legend()
+    fig2.tight_layout()
+    fig2.savefig('NeueTote-Tage_%dTageDurchschnitt.png'%avg_days)
